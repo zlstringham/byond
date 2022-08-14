423ae4a8675958dc6aa69320ce424685c9672062
Zac Stringham <zlstringham@gmail.com>
2022-08-13 19:48:46 -0700

    Error message cleanup

--- lib/rsc/src/error.rs
+++ lib/rsc/src/error.rs
@@ -6,7 +6,7 @@ use thiserror::Error;
 pub enum DecodeError {
     #[error("unexpected block flag {0} (expected 0 or 1)")]
     UnexpectedBlockFlag(u8),
-    #[error("size limit exceeded (size of {0} exceeds maximum {})", isize::MAX)]
+    #[error("size limit exceeded (size {0} exceeds maximum {})", isize::MAX)]
     SizeLimitExceeded(u32),
     #[error("checksum mismatch (expected {expected:#010x}, actual {actual:#010x})")]
     ChecksumMismatch { expected: u32, actual: u32 },
